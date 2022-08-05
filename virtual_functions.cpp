8b1e503d5485e8191334f8d15a248836ce83c12b
Momen K Tageldeen <themonx11@gmail.com>
2022-08-05 21:55:09 +0100

    Create virtual_functions.cpp

---
+++ virtual_functions.cpp
@@ -0,0 +1,31 @@
+#include <iostream>  
+using namespace std;  
+class Animal {  
+    public:  
+   virtual void function(){    
+cout<<"Eating..."<<endl;    
+    }      
+};   
+class Man: public Animal    
+{    
+ public:  
+ void function()    
+    {    
+       cout<<"Walking ..."<<endl;    
+    }    
+};  
+int main(void) {  
+    
+    Animal *p[2];
+    p[0] = new Animal();
+    p[1] = new Man();
+    p[0]->function();
+    p[1]->function();
+    // Below works without virtual function
+//  Animal A =
+//   A.function();   //parent class object 
+// //   Man m = Man();    
+//   m.function();   // child class object
+   
+   return 0;  
+}
